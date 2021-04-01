b12d7db54e6042d567de27ded474ecc8173e7597
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-04-01 12:59:19 +0200

    leon

--- Assets/TARDIS/Scripts/Camera.cs
+++ Assets/TARDIS/Scripts/Camera.cs
@@ -4,34 +4,104 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
-    private const float RADIUS = 4.0f;
-    private const float POSITION_Y = 4.5f;
-    private const float ROTATION_X = 22.5f;
+    private enum State { DOOR, AROUND, CATWALK };
 
-    private enum State {Circle};
-    private State currentState;
+    public float speed = 1.0f;
+
     private float angle;
+    private State currentState;
 
     // Start is called before the first frame update
     void Start()
     {
-        angle = 0.0f;
+        angle = Mathf.PI / 2;
+        currentState = State.DOOR;
     }
 
     // Update is called once per frame
     void Update()
     {
-        float position_x = -1 * RADIUS * Mathf.Cos(angle);
-        float position_z = RADIUS * Mathf.Sin(angle);
-        float rotation_y = 90.0f + (angle * 180 / Mathf.PI);
-        transform.position = new Vector3(position_x, POSITION_Y, position_z);
-        transform.rotation = Quaternion.Euler(ROTATION_X, rotation_y, 0.0f);
-
-        angle += (Mathf.PI / 180) / 100;
+        switch (currentState)
+        {
+            case State.DOOR:
+                Door();
+                break;
+            case State.AROUND:
+                Around();
+                break;
+            case State.CATWALK:
+                CatWalk();
+                break;
+        }
     }
 
-    private void Circle()
+    void Door()
+    {
+        float CENTER_X = -4.8f;
+        float CENTER_Y = 3.0f;
+        float CENTER_Z = -24.0f;
+        float RADIUS = 24.0f;
+        float SHIFT_X = -Mathf.PI / 32.0f;
+        float SHIFT_Z = 0.0f;
+
+        transform.position = GetCirclePosition(CENTER_X, CENTER_Y, CENTER_Z, RADIUS, angle, SHIFT_X, SHIFT_Z);
+        transform.rotation = GetCircleBehindRotation(angle, SHIFT_X, SHIFT_Z);
+        angle += (Mathf.PI / 135.0f) * Time.deltaTime;
+
+        if (angle > 2.0f)
+        {
+            currentState = State.AROUND;
+            angle = 0.0f;
+        }
+    }
+
+    void Around()
+    {
+        float CENTER_X = 0.0f;
+        float CENTER_Y = 4.5f;
+        float CENTER_Z = 0.0f;
+        float RADIUS = 4.0f;
+        float SHIFT_X = 0.0f;
+        float SHIFT_Z = 0.0f;
+
+        transform.position = GetCirclePosition(CENTER_X, CENTER_Y, CENTER_Z, RADIUS, angle, SHIFT_X, SHIFT_Z);
+        transform.rotation = FixCenter(angle, CENTER_X, CENTER_Y, 3.0f, SHIFT_X, SHIFT_Z);
+        angle += (Mathf.PI / 50.0f) * Time.deltaTime;
+    }
+
+    void CatWalk()
     {
 
+    }
+
+    Vector3 GetCirclePosition(float center_x, float center_y, float center_z, float radius, float angle, float shift_x, float shift_z)
+    {
+        Vector3 result = new Vector3(0.0f, 0.0f, 0.0f);
+
+        result.x = -1 * radius * Mathf.Cos(angle) + center_x;
+        result.y = Mathf.Cos(angle) * radius * Mathf.Sin(shift_x) + Mathf.Sin(angle) * radius * Mathf.Sin(shift_z) + center_y;
+        result.z = radius * Mathf.Sin(angle) + center_z;
+
+        return result;
+    }
+
+    Quaternion GetCircleBehindRotation(float angle, float shift_x, float shift_z)
+    {
+        float rtod = 180 / Mathf.PI;
+        float x = -Mathf.Sin(angle) * shift_x;
+        float y = angle - Mathf.PI;
+        float z = Mathf.Cos(angle) * shift_z;
+
+        return Quaternion.Euler(x * rtod, y * rtod, z * rtod);
+    }
+
+    Quaternion FixCenter(float angle, float point_x, float point_y, float point_z, float shift_x, float shift_z)
+    {
+        float rtod = 180 / Mathf.PI;
+        float x = Mathf.PI / 11;
+        float y = angle + Mathf.PI / 2;
+        float z = Mathf.Cos(angle) * shift_z;
+
+        return Quaternion.Euler(x * rtod, y * rtod, z * rtod);
     }
 }
