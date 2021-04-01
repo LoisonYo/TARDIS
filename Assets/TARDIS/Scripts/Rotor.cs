dd27b22800e0c16168d40f411f125b263a47eb6b
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-04-01 15:14:22 +0200

    new version

---
+++ Assets/TARDIS/Scripts/Rotor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotor : MonoBehaviour
+{
+    private float step;
+    public bool clockwise = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        step = (Mathf.PI / 180.0f) * 6;
+        step *= (clockwise) ? -1 : 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0.0f, 0.0f, step);
+    }
+}
