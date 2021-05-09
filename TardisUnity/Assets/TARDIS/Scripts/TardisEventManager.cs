b118ce362d380643b33dc51f48c58c21a18387d4
LoisonYo <15814178+LoisonYo@users.noreply.github.com>
2021-05-09 15:35:08 +0200

    restructure

---
+++ TardisUnity/Assets/TARDIS/Scripts/TardisEventManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TardisEventManager : MonoBehaviour
+{
+    private TardisSingleton singleton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        singleton = TardisSingleton.GetInstance();
+
+        StartCoroutine(TardisMainCouroutine());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(singleton.state);
+        if(singleton.state == TardisState.Stop || Input.GetKey("escape"))
+            Application.Quit();
+    }
+
+    IEnumerator TardisMainCouroutine()
+    {
+        yield return new WaitForSeconds(7);
+        singleton.state = TardisState.Starting_1;
+
+        yield return new WaitForSeconds(1);
+        singleton.state = TardisState.Starting_2;
+
+        yield return new WaitForSeconds(1);
+        singleton.state = TardisState.Starting_3;
+
+        yield return new WaitForSeconds(1);
+        singleton.state = TardisState.Starting_4;
+
+        yield return new WaitForSeconds(1);
+        singleton.state = TardisState.Started;
+
+        yield return new WaitForSeconds(1);
+        singleton.state = TardisState.Started;
+
+        yield return new WaitForSeconds(10);
+        singleton.state = TardisState.TakeOff_1;
+
+        yield return new WaitForSeconds(3);
+        singleton.state = TardisState.Flying;
+        yield return new WaitForSeconds(35);
+        singleton.state = TardisState.Stop;
+    }
+}
---
+++ TardisUnity/Assets/TARDIS/Scripts/TardisState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum TardisState
+{
+    Shutdown,
+    Starting_1,
+    Starting_2,
+    Starting_3,
+    Starting_4,
+    Started,
+    TakeOff_1,
+    Flying,
+    Stop,
+}
